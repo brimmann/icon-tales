02ee97219d4b5f9328b845915f597ffaacde3d69
Rashid <rashidstanikzai.net@gmail.com>
2025-06-11 13:27:48 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body mx-auto">
+          <h2 className="card-title">Hello World!</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
